5e0d721818efc183559f0cf78de2492160b27ab2
Mari <missionariamariros@gmail.com>
2024-07-13 10:18:46 -0300

    envio

---
+++ services/api.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { createClient } from '@supabase/supabase-js';
+
+/*export const supabaseUrl = 'https://iiphogydzuvawxszrzwp.supabase.co'
+export const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlpcGhvZ3lkenV2YXd4c3pyendwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA4MTE5NTgsImV4cCI6MjAzNjM4Nzk1OH0.slpRe-LR4Qb1Us9usXfY_3pymsAdbNTCeMUZP6FPzUc'
+export const client = createClient(supabaseUrl, supabaseKey)
+
+export const api  = axios.create({
+    baseURL: 'https://iiphogydzuvawxszrzwp.supabase.co',
+
+    headers: {
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlpcGhvZ3lkenV2YXd4c3pyendwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA4MTE5NTgsImV4cCI6MjAzNjM4Nzk1OH0.slpRe-LR4Qb1Us9usXfY_3pymsAdbNTCeMUZP6FPzUc",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlpcGhvZ3lkenV2YXd4c3pyendwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA4MTE5NTgsImV4cCI6MjAzNjM4Nzk1OH0.slpRe-LR4Qb1Us9usXfY_3pymsAdbNTCeMUZP6FPzUc"
+    }
+})*/
+
+export const supabaseUrl = 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1'
+export const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA'
+export const client = createClient(supabaseUrl, supabaseKey)
+
+export const api  = axios.create({
+    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+
+    headers: {
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+    }
+})
